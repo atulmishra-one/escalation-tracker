7611921208a9b91aed931b487c40bac7eae6c7ad
Atul <amishra1@q3tech.com>
2018-03-09 17:41:43 +0530

    Make profile model

--- escalationtracker/urls.py
+++ escalationtracker/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', auth_views.login, {'template_name': 'login.html'}, name='login'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('accounts/profile/', views.profile)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
